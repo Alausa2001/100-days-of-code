f9f33d84d27c34a73b022ca293b4aa885e4e547a
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-06-19 15:56:18 +0100

    100days of code: python

---
+++ my_codes/challenge2.py
@@ -0,0 +1,6 @@
+#this program takes in the age of the user and add
+#the first two digit
+age = input("type in your age(in qoutes: for example '33' not 33\n")
+first_digit = int(age[0])
+second_digit= int(age[1])
+print((first_digit) + (second_digit))
