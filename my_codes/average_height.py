8ad2b1848af0f35064e7d4fffd27a3c1cfe5104a
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-06-26 20:42:41 +0100

    modified the code: implemented an algo. for len() and sum()

--- my_codes/average_height.py
+++ my_codes/average_height.py
@@ -5,7 +5,11 @@ student = (input("Enter the list of student height: ")).split(", ")
 for n in range(0, len(student)):
     student[n] = float(student[n])
 
-for p in range(0, n+1):
-    sum += student[p]
-average = round(sum / (n+1), 2)
+sum = 0
+for height in student:
+    sum += height
+length = 0
+for height in student:
+    length += 1
+average = round(sum / length, 2)
 print(f"Their average height is {average}")
